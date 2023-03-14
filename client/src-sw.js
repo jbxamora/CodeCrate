de71a84eda72eff0e69d14e2bd5cc0c9c406ef18
jbxamora <jbxamora@icloud.com>
2023-03-13 17:53:16 -0700

    added comments to src + offline fallback

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,12 +1,15 @@
+// Import required modules
 const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
 const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
+// Precache and route all assets
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Create a CacheFirst strategy for pages
 const pageCache = new CacheFirst({
   cacheName: "page-cache",
   plugins: [
@@ -14,19 +17,21 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
     }),
   ],
 });
 
+// Warm the page cache by pre-caching important pages
 warmStrategyCache({
   urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
+// Register a route for page navigation requests
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// Implement asset caching
+// Implement asset caching using StaleWhileRevalidate strategy
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
@@ -38,3 +43,10 @@ registerRoute(
     ],
   })
 );
+
+// Register a fallback strategy for offline content
+offlineFallback({
+  // Use a fallback URL when the user is offline
+  url: '/index.html',
+});
+
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,50 +1,50 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 // Initialize the database
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
       // Check if the object store already exists
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
       // Create a new object store with auto-incrementing keys
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // PUT function to add data to the database
 export const putDb = async (id, value) => {
-  console.log('PUT request to update the jateDB');
+  console.log("PUT request to update the jateDB");
   // Connect to the database
-  const jateDb = await openDB('jate', 1);
+  const jateDb = await openDB("jate", 1);
   // Create a new transaction with read-write privileges
-  const tx = jateDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction("jate", "readwrite");
   // Open the object store
-  const objStore = tx.objectStore('jate');
+  const objStore = tx.objectStore("jate");
   // Use the .put() method to add content to the object store
   const req = objStore.put({ id: id, value: value });
   // Wait for the request to complete and log the result
   const res = await req;
-  console.log('data saved to the jateDB', res);
+  console.log("data saved to the jateDB", res);
 };
 
 // GET function to retrieve all data from the database
 export const getDb = async () => {
-  console.log('Getting data from the jateDB');
+  console.log("Getting data from the jateDB");
   // Connect to the database
-  const jateDb = await openDB('jate', 1);
+  const jateDb = await openDB("jate", 1);
   // Create a new transaction with read-only privileges
-  const tx = jateDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction("jate", "readonly");
   // Open the object store
-  const objStore = tx.objectStore('jate');
+  const objStore = tx.objectStore("jate");
   // Use the .getAll() method to retrieve all content from the object store
   const req = objStore.getAll();
   // Wait for the request to complete and log the result
   const res = await req;
-  console.log('data retrieved from the jateDB', res);
+  console.log("data retrieved from the jateDB", res);
 };
 
 // Initialize the database when the module is imported

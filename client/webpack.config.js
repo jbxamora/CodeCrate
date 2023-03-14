0f89c97c88a6a4722d714730eb79abe5e04cd941
jbxamora <jbxamora@icloud.com>
2023-03-13 17:15:19 -0700

    formatted files using prettier

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,57 +1,56 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     devServer: {
       // The `hot` option is to use the webpack-dev-server in combination with the hot module replacement API.
-      hot: 'only',
+      hot: "only",
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE'
+        template: "./index.html",
+        title: "JATE",
       }),
       new WebpackPwaManifest({
-        name: 'JATE',
-        short_name: 'JATE',
-        description: 'Just Another Text Editor!',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
+        name: "JATE",
+        short_name: "JATE",
+        description: "Just Another Text Editor!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
     ],
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
-
       ],
     },
   };
